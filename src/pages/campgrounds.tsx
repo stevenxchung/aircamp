f4add1dc433a1401215318e611f2df79f0d93858
Steven <stevenxchung@gmail.com>
2023-06-09 23:38:07 -0400

    Try initial react-bootstrap navbar

--- src/pages/campgrounds.tsx
+++ src/pages/campgrounds.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import CustomNavbar from "~/components/navbar";
 
 const Campgrounds: NextPage = () => {
   return (
@@ -7,6 +8,7 @@ const Campgrounds: NextPage = () => {
       <Head>
         <title>Campgrounds</title>
       </Head>
+      <CustomNavbar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="text-4xl">Campgrounds</h1>
       </main>
