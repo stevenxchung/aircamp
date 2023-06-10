50b4c76ff9f740002828bba83fca227f173af182
Steven <stevenxchung@gmail.com>
2023-06-10 12:28:54 -0400

    Update navbar.tsx
    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,14 +4,14 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-function CollapsibleExample() {
+const CustomNavbar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">AirCamp</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ml-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
@@ -23,6 +23,6 @@ function CollapsibleExample() {
       </Container>
     </Navbar>
   );
-}
+};
 
-export default CollapsibleExample;
+export default CustomNavbar;
