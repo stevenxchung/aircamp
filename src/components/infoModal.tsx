31bd266418002288ec81237033bfef359629f10f
Steven <stevenxchung@gmail.com>
2023-06-14 14:01:16 -0400

    Migration | Initial Migration to Next.js + Vercel (#4)
    * Migrate to T3 initial commit
    
    * Add images and typewriter FX to landing page
    
    * Update packages and configs
    
    * Change default font to DeepMind
    
    * Try initial react-bootstrap navbar
    
    * Update navbar.tsx
    
    * Add initial AirCamp models
    
    * Update navbar.tsx
    
    * Move bootstrap styles
    
    Update globals.css
    
    * Update navbar.tsx
    
    Update navbar.tsx
    
    * Add packages
    
    * Add DB seed script
    
    * Add bootstrap navbar and campgrounds
    
    * Refactor styles
    
    * Create infoModal.tsx
    
    * Update campgrounds.tsx
    
    * Update campgrounds.tsx

---
+++ src/components/infoModal.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import ReactModal from "react-modal";
+
+interface InfoModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+const InfoModal: React.FC<InfoModalProps> = ({ isOpen, closeModal }) => {
+  const modalStyles = {
+    content: {
+      width: "50%",
+      height: "25%",
+      margin: "auto",
+      background: "#f5f5f5",
+      border: "none",
+      borderRadius: "8px",
+      padding: "20px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+    },
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+  };
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      style={modalStyles as ReactModal.Styles}
+      shouldCloseOnOverlayClick={false}
+    >
+      <h2>Note</h2>
+      <p>
+        Currently doing migration on the legacy app, to view the legacy version
+        see:&nbsp;
+        <Link href="https://github.com/stevenxchung/AirCamp-App/releases/tag/v0.0.1">
+          legacy-ejs-nodejs-app
+        </Link>
+        &nbsp;and the corresponding README for more information on how to run
+        the app locally.
+      </p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <button onClick={closeModal}>
+          <h4>OK</h4>
+        </button>
+      </div>
+    </ReactModal>
+  );
+};
+
+export default InfoModal;
