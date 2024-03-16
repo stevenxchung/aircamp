eeb490eb86f9931c70ef06c023881cae6f9fb85a
Steven <stevenxchung@gmail.com>
2024-03-15 22:24:01 -0400

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import { prisma } from "../src/server/db";
 const main = async () => {
   const mockUsers = [
     {
-      airCampUserId: process.env.USER_ID,
+      airCampUserId: process.env.USER_ID ?? "001",
       username: "DeepLearner",
       firstName: "Steven",
       lastName: "C",
@@ -14,7 +14,7 @@ const main = async () => {
 
   const mockCampgrounds = [
     {
-      airCampUserId: process.env.USER_ID,
+      airCampUserId: process.env.USER_ID ?? "001",
       name: "Cloud's Rest",
       price: "666",
       summary:
@@ -26,7 +26,7 @@ const main = async () => {
       lng: -119.4894,
     },
     {
-      airCampUserId: process.env.USER_ID,
+      airCampUserId: process.env.USER_ID ?? "001",
       name: "Yellow Mountain",
       price: "777",
       summary:
@@ -38,7 +38,7 @@ const main = async () => {
       lng: 118.3387,
     },
     {
-      airCampUserId: process.env.USER_ID,
+      airCampUserId: process.env.USER_ID ?? "001",
       name: "Shangri-La",
       price: "888",
       summary:
@@ -50,7 +50,7 @@ const main = async () => {
       lng: 99.7432,
     },
     {
-      airCampUserId: process.env.USER_ID,
+      airCampUserId: process.env.USER_ID ?? "001",
       name: "Nusa Penida",
       price: "999",
       summary:
