054c0bb4552c899a2d11842646047ea47ccc6ea8
Steven <stevenxchung@gmail.com>
2024-03-16 08:39:36 -0400

    Migration | Planet Scale to Turso (#9)
    * Migrate to Turso
    
    * Update seed.ts
    
    * Update ci.yml
    
    Update ci.yml
    
    * Update package-lock.json

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    DATABASE_TOKEN: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
@@ -43,6 +44,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_TOKEN: process.env.DATABASE_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
